1ef2478265f99bbd8f901897190f02fbda9ae326
Johannes Erwerle <jo@swagspace.org>
2022-04-05 00:40:31 +0200

    fixed module comment in cisco_ios module

--- network_hash_gen/cisco_ios/__init__.py
+++ network_hash_gen/cisco_ios/__init__.py
@@ -1,8 +1,8 @@
-__pdoc__ = {}
-__pdoc__["tests"] = False
-
 """
 This module provides hash functions for Cisco IOS and IOS-XE.
 
 Currently only type 5 (md5-based) and type 9 (scrypt-based) are supported.
 """
+
+__pdoc__ = {}
+__pdoc__["tests"] = False
