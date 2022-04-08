0cd106a93285aedb204a0918b1268b191ecc8211
Johannes Erwerle <jo@swagspace.org>
2022-04-08 16:22:45 +0200

    Closes #14: replaced the crypt library with passlib

--- network_hash_gen/juniper_junos/type_6.py
+++ network_hash_gen/juniper_junos/type_6.py
@@ -1,13 +1,21 @@
-from crypt import crypt
+from passlib.hash import sha512_crypt
 
 from network_hash_gen.base import BaseHash
 
 
 class Type6(BaseHash):
+    """
+    SHA2 512 bit based hash for Juniper JunOS.
+
+    Specification: https://akkadia.org/drepper/SHA-crypt.txt
+    Details:
+    - 5000 rounds are used
+    - the round specifier is omitted.
+    """
 
     salt_length = 8
     salt_chars = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
     @staticmethod
     def hash_salted(password: str, salt: str) -> str:
-        return crypt(password, f"$6${salt}")
+        return sha512_crypt.using(salt=salt, rounds=5000).hash(password)
